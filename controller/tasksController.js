1ff116e98e184efcbc8245c14cd3fceb6779acd7
30Xp107 <39079475+30Xp107@users.noreply.github.com>
2023-06-03 08:38:05 +0000

    Fix Update Loading

--- controller/tasksController.js
+++ controller/tasksController.js
@@ -36,13 +36,18 @@ const deleteTask = asyncWrapper(async (req, res, next) => {
 })
 
 const updateTask = asyncWrapper(async (req, res) => {
-    const {id:taskID} = req.params
-    const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
-        new: true,
-        runValidators: true,
-    })
-    if(!task) {
-        return next(createCustomError(`No task with id: ${taskID}`, 404))
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        if(!task) {
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
     }
 })
 
